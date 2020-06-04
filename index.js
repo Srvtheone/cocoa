1915d242df83a3cae3b8a3c912ce9f02de81cabc
Saurav Anand <anandsaurav6@gmail.com>
2020-06-04 09:15:16 +0530

    Text Update Working

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3500;
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  let response = { data: "Bye Bye" };
+  let response = {
+    title: "Next-Gen CMS",
+    content: [
+      { attr: "para1", content: "THis is the first paragraph" },
+      { attr: "para2", content: "thIS is the second paragraph" },
+    ],
+  };
   res.status(200).json(response);
 });
 

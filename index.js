2673d0ed8e0d7e6c18f171f3a71b4ef3f1605b3c
Saurav Anand <anandsaurav6@gmail.com>
2020-06-06 03:08:49 +0530

    Set Text Working Properly and Example updated

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ app.use(express.json());
 app.get("/", (req, res) => {
   let response = {
     title: "Next-Gen CMS",
-    content: [
+    text: [
       { attr: "para1", content: "THis is the first paragraph" },
       { attr: "para2", content: "thIS is the second paragraph" },
+    ],
+    attributes: [
+      { attr: "title", value: "This element has a dynamic title" },
+      { attr: "href", value: "This is a dynamic link" },
     ],
   };
   res.status(200).json(response);

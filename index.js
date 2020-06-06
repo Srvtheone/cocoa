a411c8d12240009ff2cd82df9500b8b5001b9f46
Saurav Anand <anandsaurav6@gmail.com>
2020-06-07 04:41:24 +0530

    Custom Image Tags Added

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.get("/", (req, res) => {
       { id: "para2", content: "thIS is the second paragraph" },
       { id: "para3", content: "Road to dynamism" },
       { id: "multi", content: "Testing If Multiple Attributes Work?" },
+      { id: "imgtxt1", content: "The image above is dynamic too!" },
     ],
     attributes: [
       {
@@ -28,6 +29,13 @@ app.get("/", (req, res) => {
       { id: "multi-attr1", attribute: "style", value: "color: red;" },
       { id: "attr5", attribute: "target", value: "_blank" },
       { id: "attr6", attribute: "style", value: "font-size: 44px;" },
+    ],
+    images: [
+      {
+        id: "img1",
+        src: "https://via.placeholder.com/150x150.png",
+        alt: "Dynamic Image",
+      },
     ],
   };
   res.status(200).json(response);
